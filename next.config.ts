da80b8edf2ad5943dac83db6ccae4604a3e6ca1d
kamil-tokarz <kamil.tokarz@gmail.com>
2026-08-21 16:45:15 +0200

    refactor: remove obsolete server actions config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["**.app.github.dev", "*.app.github.dev", "localhost:3000"],
-    },
-  },
 };
 
 export default nextConfig;
